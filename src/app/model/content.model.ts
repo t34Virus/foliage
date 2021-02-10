4d71f268ffea160ac6dd319727b5ff654eb0aff4
treindle <teresa.nededog@spinifexgroup.com>
2021-02-09 16:24:21 -0800

    Add: model and project config

---
+++ src/app/model/content.model.ts
@@ -0,0 +1,12 @@
+export interface ContentSetup {
+    name: string;
+    thumb: string;
+    poster: string;
+    demo: string;
+    embed: string;
+    copy: string;
+    platform: string;
+    url: string;
+    tech: Array<number>;
+    active: boolean;
+}
