4f0da824d2e6358db480481cc3edaf96de442c2b
treindle <teresa.nededog@spinifexgroup.com>
2021-02-10 20:54:46 -0800

    Add: about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 import { LandingPageComponent } from './components/pages/landing-page/landing-page.component';
 import { WorkPageComponent } from './components/pages/work-page/work-page.component';
+import { AboutPageComponent } from './components/pages/about-page/about-page.component';
 
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: 'work', component: WorkPageComponent }
+  { path: 'work', component: WorkPageComponent },
+  { path: 'about', component: AboutPageComponent }
 
 ]
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
